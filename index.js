69b27627a6d4b60447a74f6334cfb332fb61b250
Vladimir Chekushkin <chekushkin@orbsoft.ru>
2018-12-03 18:58:17 +0300

    fix

--- index.js
+++ index.js
@@ -5,25 +5,19 @@ var path = require('path');
 const PORT = process.env.PORT || 8080;
 const app = express();
 
-app.use(express.static(path.join(__dirname + 'dist')));
-
 app.get('*', function (req, res) {
   res.sendFile(path.join(__dirname + 'dist/index.html'));
 });
 
-app.set('port', PORT);
-
 // const distDir = __dirname + "/dist";
-// const server = express()
-//     .use(function (req, res) {
-//         res.send('<h2>Hello world from Server222 TEst!</h2>');
-//     })
-//     .listen(PORT, function () {
-//         console.log('app is running on port: ' + PORT);
-//     })
-// ;
-//
-const io = socketIO(app);
+const server = express()
+    .use(express.static(__dirname + 'dist'))
+    .listen(PORT, function () {
+        console.log('app is running on port: ' + PORT);
+    })
+;
+
+const io = socketIO(server);
 
 io.on('connection', function (socket) {
    console.log('Client connected');
