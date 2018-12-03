b10e52ca5440ad898b25da5493e8e8546b1d2d4e
Vladimir Chekushkin <chekushkin@orbsoft.ru>
2018-12-03 16:47:34 +0300

    index page

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log("Test");
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function (req, res) {
+    // res.header('Access-Control-Allow-Credentials', false);
+    res.send('<h2>Hello world from Server!</h2>');
+});
