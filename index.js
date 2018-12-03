98510a250cd4691071febb91333ab2ea021b972d
Vladimir Chekushkin <chekushkin@orbsoft.ru>
2018-12-03 17:08:32 +0300

    import socket.io

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 var express = require('express');
-// var io = require('socket.io')();
+var io = require('socket.io');
 
 var app = express();
 var port = process.env.PORT || 8080;
+io(app);
 
 app.get('/', function (req, res) {
     res.send('<h2>Hello world from Server!</h2>');
@@ -10,4 +11,13 @@ app.get('/', function (req, res) {
 
 app.listen(port, function () {
    console.log('app is running on port: ' + port);
+});
+
+io.on('connection', function (socket) {
+   console.log('Client connected');
+
+   // Disconnect
+   socket.on('disconnect', function () {
+       console.log('Client disconnected');
+   })
 });
