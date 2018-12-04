b9eb856a5bcbe9dddde1015fb5ac0844d1a71b49
Vladimir Chekushkin <chekushkin@orbsoft.ru>
2018-12-04 11:28:46 +0300

    change package.json

--- index.js
+++ index.js
@@ -5,24 +5,28 @@ var path = require('path');
 const PORT = process.env.PORT || 8080;
 const app = express();
 
-app.get('/', function (req, res) {
+app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname + 'dist/index.html'));
 });
 
-const server = express()
-    .use(express.static(__dirname + 'dist'))
-    .listen(PORT, function () {
-        console.log('app is running on port: ' + PORT);
-    })
-;
-
-const io = socketIO(server);
-
-io.on('connection', function (socket) {
-   console.log('Client connected');
-
-   // Disconnect
-   socket.on('disconnect', function () {
-       console.log('Client disconnected');
-   })
+app.listen(PORT, function () {
+  console.log('app is running on port: ' + PORT);
 });
+
+// const server = express()
+//     .use(express.static(__dirname + 'dist'))
+//     .listen(PORT, function () {
+//         console.log('app is running on port: ' + PORT);
+//     })
+// ;
+//
+// const io = socketIO(server);
+//
+// io.on('connection', function (socket) {
+//    console.log('Client connected');
+//
+//    // Disconnect
+//    socket.on('disconnect', function () {
+//        console.log('Client disconnected');
+//    })
+// });
