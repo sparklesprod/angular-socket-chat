fb7a725a69a9788118fdfa95a21f733013d20dbb
Vladimir Chekushkin <chekushkin@orbsoft.ru>
2018-12-04 11:39:23 +0300

    index.js with socket.io

--- index.js
+++ index.js
@@ -5,30 +5,30 @@ var path = require('path');
 const PORT = process.env.PORT || 8080;
 const app = express();
 
-app.use(express.static(__dirname + '/dist'));
+// app.use(express.static(__dirname + '/dist'));
 
-app.listen(PORT, function () {
-  console.log('app is running on port: ' + PORT);
-});
+// app.listen(PORT, function () {
+//   console.log('app is running on port: ' + PORT);
+// });
 
 app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
-// const server = express()
-//     .use(express.static(__dirname + 'dist'))
-//     .listen(PORT, function () {
-//         console.log('app is running on port: ' + PORT);
-//     })
-// ;
-//
-// const io = socketIO(server);
-//
-// io.on('connection', function (socket) {
-//    console.log('Client connected');
-//
-//    // Disconnect
-//    socket.on('disconnect', function () {
-//        console.log('Client disconnected');
-//    })
-// });
+const server = express()
+    .use(express.static(__dirname + '/dist'))
+    .listen(PORT, function () {
+        console.log('app is running on port: ' + PORT);
+    })
+;
+
+const io = socketIO(server);
+
+io.on('connection', function (socket) {
+   console.log('Client connected');
+
+   // Disconnect
+   socket.on('disconnect', function () {
+       console.log('Client disconnected');
+   })
+});
