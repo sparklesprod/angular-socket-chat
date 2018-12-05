dcf98a111f7a89a50a86eadafedcf6eba524c3a0
Vladimir Chekushkin <chekushkin@orbsoft.ru>
2018-12-05 14:17:42 +0300

    animation module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from "@angular/forms";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 
 import {AppComponent} from './app.component';
@@ -14,6 +15,7 @@ import {CommonModule} from "@angular/common";
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     FormsModule,
     CommonModule
   ],
