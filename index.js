a38e2a28d9786e0c087d7c14e0b3e3bdba30ff93
Vladimir Chekushkin <chekushkin@orbsoft.ru>
2018-12-05 12:23:50 +0300

    fix

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ io.on('connection', function (socket) {
   socket.on('message', function (message) {
     console.log("[Сервер]: ", message);
     console.log("new Date: ", new Date);
-    console.log("new Date.getTime(): ", new Date.getTime());
+    console.log("new Date.getTime(): ", new Date().getTime());
     io.emit('message', message);
   });
 
