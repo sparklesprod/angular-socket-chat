3864630901f0188cce1a533fac3eb79795946bb1
Vladimir Chekushkin <chekushkin@orbsoft.ru>
2018-12-05 11:59:00 +0300

    app.comp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,6 @@
-import {AfterViewInit, Component, ElementRef, OnInit, QueryList, ViewChild, ViewChildren} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, OnDestroy, OnInit, QueryList, ViewChild, ViewChildren} from '@angular/core';
 import {NgForm} from "@angular/forms";
 import {WebsocketService} from "./services/websocket.service";
-import {OnlineEvent} from "./classes/online-event";
-import {Event} from "./shared/model/event";
 import {Message} from "./shared/model/Message";
 import {User} from "./shared/model/User";
 import {Observable} from "rxjs/Observable";
@@ -12,11 +10,13 @@ import {Observable} from "rxjs/Observable";
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less']
 })
-export class AppComponent implements OnInit, AfterViewInit {
+export class AppComponent implements OnInit, OnDestroy, AfterViewInit {
   public user: User;
   public online: Observable<number>;
   public messages: Message[] = [];
   public message: string;
+
+  private _connection;
 
   @ViewChild('form') form: NgForm;
   @ViewChildren('chatMsg', {read: ElementRef}) chatMessage: QueryList<ElementRef>;
@@ -25,15 +25,14 @@ export class AppComponent implements OnInit, AfterViewInit {
   {
     this.initUserModel();
     this.initIoConnection();
+
+    this.online = this.wsService.online();
   }
 
-  ngOnInit() {
-    this.online = this.wsService.online();
-    console.log('Подключенных юзеров: ', this.online);
-    // this.wsService.online().subscribe((onlineUsers: OnlineEvent) => {
-    //   this.online = onlineUsers.online;
-    //   // console.log('Подключенных юзеров: ', this.online);
-    // })
+  ngOnInit() {}
+
+  ngOnDestroy() {
+    this._connection.unsubscribe();
   }
 
   ngAfterViewInit(): void {
@@ -51,7 +50,6 @@ export class AppComponent implements OnInit, AfterViewInit {
       return;
     }
 
-    // console.log('Сообщение: ', this.message);
     this.wsService.send({
       from: this.user,
       content: this.message
@@ -64,23 +62,21 @@ export class AppComponent implements OnInit, AfterViewInit {
     this.user = {
       id: randomId
     };
-
-    // console.log('Init User: ', this.user);
   }
 
   private initIoConnection(): void {
     this.wsService.initSocket();
 
-    this.wsService.onMessage().subscribe((message: Message) => {
+    this._connection = this.wsService.onMessage().subscribe((message: Message) => {
       this.messages.push(message);
     });
 
-    this.wsService.onEvent(Event.CONNECT).subscribe(() => {
-      console.log('connected');
-    });
+    // this.wsService.onEvent(Event.CONNECT).subscribe(() => {
+    //   console.log('connected');
+    // });
 
-    this.wsService.onEvent(Event.DISCONNECT).subscribe(() => {
-      console.log('disconnect');
-    })
+    // this.wsService.onEvent(Event.DISCONNECT).subscribe(() => {
+    //   console.log('disconnect');
+    // });
   }
 }
